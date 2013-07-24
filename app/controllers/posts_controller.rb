72034a976da5d832689a1a7ac60a1a7331bfb7fc
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-07-24 15:36:15 +0900

    Implement posts list

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,26 @@
 class PostsController < UITableViewController
+  def viewDidLoad
+    @posts = []
+    BW::HTTP.get('http://localhost:3000/api/v1/posts.json') do |response|
+      if response.ok?
+        json = BW::JSON.parse(response.body.to_s)
+        @posts += json
+        tableView.reloadData
+      else
+        App.alert(response.error_message)
+      end
+    end
+  end
 
+  def tableView(table_view, numberOfRowsInSection: section)
+    @posts.count
+  end
+
+  def tableView(table_view, cellForRowAtIndexPath: index_path)
+    cell = table_view.dequeueReusableCellWithIdentifier('post_cell', forIndexPath: index_path)
+    post = @posts[index_path.row]
+    cell.textLabel.text = post['title']
+    cell.detailTextLabel.text = post['body']
+    cell
+  end
 end
