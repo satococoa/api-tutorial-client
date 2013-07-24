8b2b33be9ac6046b3fbca4b22b1fd2e095216d2e
Satoshi Ebisawa <e.satoshi@gmail.com>
2013-07-24 15:12:05 +0900

    Add PostsController

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,6 +1,8 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     @window = UIWindow.alloc.initWithFrame(App.bounds)
+    storyboard = UIStoryboard.storyboardWithName('Storyboard', bundle: nil)
+    @window.rootViewController = storyboard.instantiateInitialViewController
     @window.makeKeyAndVisible
     true
   end
